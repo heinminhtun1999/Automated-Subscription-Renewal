454654bac4e4ce211cdcaa99d7bb6913f1431f3e
heinminhtun <heinminhtun27@gmail.com>
2026-05-18 17:35:49 +0800

    add dotenv config in worker.js 8

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     apps: [
         {
-            // PM2 process definition for the app.
+            // PM2 process definition for the web app.
             name: 'Automated Subscription Renewal',
             script: 'src/index.js',
+            env_file: '.env',
             env: {
                 // Default development environment variables.
                 NODE_ENV: 'development',
@@ -13,6 +14,18 @@ module.exports = {
                 // Production overrides.
                 NODE_ENV: 'production',
                 PORT: 4000,
+            }
+        },
+        {
+            // PM2 process definition for the cron worker.
+            name: 'Automated Subscription Renewal Cron',
+            script: 'src/jobs/worker.js',
+            env_file: '.env',
+            env: {
+                NODE_ENV: 'development'
+            },
+            env_production: {
+                NODE_ENV: 'production'
             }
         }
     ]
