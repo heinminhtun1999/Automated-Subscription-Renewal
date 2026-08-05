d89b9ea30b912a41912087a4dcae730b784a116c
heinminhtun <heinminhtun27@gmail.com>
2026-08-05 16:37:13 +0800

    remove uncessary .env option in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,31 +7,13 @@ module.exports = {
             // PM2 process definition for the web app.
             name: isProduction ? 'asr' : 'asr-dev',
             cwd: __dirname,
-            script: path.join(__dirname, 'src/index.js'),
-            env_file: '.env',
-            env: {
-                // Default development environment variables.
-                NODE_ENV: 'development',
-                PORT: 4000,
-            },
-            env_production: {
-                // Production overrides.
-                NODE_ENV: 'production',
-                PORT: 4000,
-            }
+            script: path.join(__dirname, 'src/index.js')
         },
         {
             // PM2 process definition for the cron worker.
             name: isProduction ? 'asr-crons' : 'asr-dev-crons',
             cwd: __dirname,
-            script: path.join(__dirname, 'src/jobs/worker.js'),
-            env_file: '.env',
-            env: {
-                NODE_ENV: 'development'
-            },
-            env_production: {
-                NODE_ENV: 'production'
-            }
+            script: path.join(__dirname, 'src/jobs/worker.js')
         }
     ]
 }
