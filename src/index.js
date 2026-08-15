f2e451e229d8f4783d0e30f4041f666e3576b353
heinminhtun <heinminhtun27@gmail.com>
2026-08-15 12:36:21 +0800

    comment the cron job test in index.js

--- src/index.js
+++ src/index.js
@@ -102,12 +102,12 @@ app.use((req, res, next) => {
 
 
 // ================== Reconciliation in development environment ==================
-if (process.env.NODE_ENV === 'development') {
-    const runCronJobs = require('./jobs/generalJobsRunner');
-    setInterval(() => {
-        runCronJobs();
-    }, 5 * 60 * 1000); // Run every 5 minutes
-}
+// if (process.env.NODE_ENV === 'development') {
+//     const runCronJobs = require('./jobs/generalJobsRunner');
+//     setInterval(() => {
+//         runCronJobs();
+//     }, 5 * 60 * 1000); // Run every 5 minutes
+// }
 // ===============================================================================
 
 app.get('/', (req, res) => {
@@ -124,8 +124,8 @@ app.get('/admin/login', renderLoginPage);
 app.post('/admin/login', limiter, handleAuth);
 
 // Middleware to check if the user is authorized to access admin routes
-// app.use('/admin', isAuthenticated);
-// app.use('/api', isAuthenticated);
+app.use('/admin', isAuthenticated);
+app.use('/api', isAuthenticated);
 
 app.get('/admin', renderHomePage);
 
